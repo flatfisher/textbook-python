6a6328205a0ef754a832e96110bdc99c6e6e83cb
Kyohei Tsuda <shimano.entou@gmail.com>
2017-02-21 14:43:57 +0900

    Add example code.

---
+++ projects/chapter1/For.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+for value in range(0,100):
+     print(str(value),"回目の処理")
---
+++ projects/chapter1/Variable.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*- 
+
+number = 100
+
+number2 = number + 10 # 加算（足し算）
+number3 = number - 10 # 減算（引き算）
+number4 = number * 10 # 乗算（掛け算）
+number5 = number / 10 # 除算（割り算）
+
+print(number2)
+print(number3)
+print(number4)
+print(number5)
+
+#型もチェックしてみます
+print(type(number2))
+print(type(number3))
+print(type(number4))
+print(type(number5))
