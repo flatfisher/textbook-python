9935f064f1852f3ef1e4851671248440035f30d1
Kyohei Tsuda <shimano.entou@gmail.com>
2017-02-21 23:40:20 +0900

    Add example.

---
+++ projects/chapter1/List.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# 文字列のリストを用意
+tohoku_list = ['Aomori','Akita','Iwate','Yamagata','Miyagi','Fukushima']
+
+for (i, value) in enumerate(tohoku_list):
+    # index指定
+    print(tohoku_list[i])
+    # value指定
+    print(value)
